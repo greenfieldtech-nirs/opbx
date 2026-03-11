9ca3011cd29a1cb16b4a6b17377d4ff39e33c6f8
Nir Simionovich <nirs@cloudonix.io>
2026-03-11 19:26:23 +0200

    fix(ivr): add missing BusinessHours import to IVR menu request classes
    - Add use App\Models\BusinessHours; to UpdateIvrMenuRequest.php
    - Add use App\Models\BusinessHours; to StoreIvrMenuRequest.php

--- app/Http/Requests/StoreIvrMenuRequest.php
+++ app/Http/Requests/StoreIvrMenuRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use App\Models\AiAssistant;
+use App\Models\BusinessHours;
 use App\Models\ConferenceRoom;
 use App\Models\Extension;
 use App\Models\IvrMenu;
